4937d8cfc547ef0ba5d790623a1cd3f05fac2548
juan <jubaga95@gmail.com>
2019-09-15 02:51:16 +0200

    Ejercicio de adivinacion, con reiteracion

--- src/Adivinacion.java
+++ src/Adivinacion.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class Adivinacion {
 
     public static void main(String[] args) {
-        int random, num, cont = 0;
+        int random, num, cont = 1, cont2 = 1;
+        double media = 0;
         String consulta = null;
-        boolean continuar = false;
+        boolean continuar = true;
         Scanner sc;
         sc = new Scanner(System.in);
         random = (int) (Math.random() * 101);
@@ -22,19 +23,49 @@ public class Adivinacion {
             else
                 cont++;
 
-            System.out.printf("Lo has intentado %d veces\n", cont);
         } while (num != random);
 
-        /* ************************************************* */
-        System.out.printf("Quieres volver a jugar: (s/n):");
-        consulta = sc.next();
-        sc.nextLine();
+        System.out.printf("Lo has intentado %d veces\n", cont);
 
-        if (consulta.toLowerCase().equals("s")) {
-            continuar = true;
-        } else {
-            continuar = false;
+
+        while (continuar) {
+            cont2 = 1;
+            System.out.printf("Quieres volver a jugar: (s/n):");
+            consulta = sc.next();
+            sc.nextLine();
+
+
+            random = (int) (Math.random() * 101);
+            System.out.println(random);
+
+
+            do {
+                System.out.printf("Introduce un numero entero entre 1 y 100: \n");
+                num = sc.nextInt();
+                sc.nextLine();
+
+                if (random == num)
+                    System.out.printf("Has adivinado el numero!\n");
+                else
+                    cont++;
+
+            } while (num != random);
+
+            System.out.printf("Lo has intentado %d veces\n", cont);
+
+
+            System.out.printf("Quieres volver a jugar: (s/n):");
+            consulta = sc.next();
+            sc.nextLine();
+
+
+            if (consulta.toLowerCase().equals("s")) {
+                continuar = true;
+            } else {
+                continuar = false;
+            }
+
         }
-        /* ************************************************* */
+        System.out.println("Hasta pronto");
     }
 }
